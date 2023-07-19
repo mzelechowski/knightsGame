22298a133d6295a7fd9db18846235f69f24fa0bf
zelek <mzelechowski79@gmail.com>
2023-07-19 09:20:40 +0200

    springsecurity

--- src/main/java/com/malarska/knightsgame/config/MainConfig.java
+++ src/main/java/com/malarska/knightsgame/config/MainConfig.java
@@ -3,10 +3,11 @@ package com.malarska.knightsgame.config;
 import com.malarska.knightsgame.domain.repository.DBKnightRepository;
 import com.malarska.knightsgame.domain.repository.InMemoryRepository;
 import com.malarska.knightsgame.domain.repository.KnightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class MainConfig {
@@ -24,6 +25,14 @@ public class MainConfig {
     public KnightRepository createDBKnightRepo(){
         KnightRepository repo = new DBKnightRepository();
         return repo;
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user1").roles("USER")
+                .and()
+                .withUser("user2").password("{noop}user2").roles("ADMIN");
     }
 
 }
